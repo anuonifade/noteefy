f47f49ad12a19241d882918369bd5c943d672381
andela-aonifade <anuoluwapo.onifade@andela.com>
2018-10-20 08:45:31 +0100

    handles notification

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { Permissions, Notifications } from 'expo';
 import FBLoginButton from './src/FBLoginButton';
 import HomePage from './src/HomePage';
 
-const PUSH_ENDPOINT = 'http://localhost:3000/push-token'
+const PUSH_ENDPOINT = 'http://noteefy-api.herokuapp.com/push-token'
 
 export default class App extends Component {
   constructor(props) {
@@ -30,30 +30,32 @@ export default class App extends Component {
   
     // Stop here if the user did not grant permissions
     if (finalStatus !== 'granted') {
+      alert('You phone is not configured to receive notification from the app');
       return;
     }
   
     // Get the token that uniquely identifies this device
     let token = await Notifications.getExpoPushTokenAsync();
+    Alert.alert('Push Notification Token',`${token}`);
   
     // POST the token to your backend server from where you can retrieve it to send push notifications.
-    return fetch(PUSH_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        token: {
-          value: token,
-        },
-        user: {
-          id: this.state.userInfo.id,
-          name: this.state.userInfo.name,
-          email: this.state.userInfo.email
-        },
-      }),
-    });
+    // const response = await fetch('http://noteefy-api.herokuapp.com/push-token', {
+    //   method: 'POST',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     token: {
+    //       value: token,
+    //     },
+    //     user: {
+    //       id: this.state.userInfo.id,
+    //       name: this.state.userInfo.name,
+    //       email: this.state.userInfo.email
+    //     },
+    //   }),
+    // });
   }
   async FBlogIn() {
     const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync('1029836687195082', {
@@ -64,16 +66,18 @@ export default class App extends Component {
       const response = await fetch(
         `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email`);
         const userInfo = await response.json();
-        this.setState({ userInfo, loggedIn: true }, () => {
-          this.registerForPushNotificationsAsync();
+        this.setState({ userInfo }, () => {
+          Alert.alert('Facebook Registration', `Thanks for registering ${userInfo.name}`);
         });
+
+        this.setState({loggedIn: true});
     }
   }
   render() {
     const {loggedIn} = this.state;
     return (
       <View style={styles.container}>
-        { loggedIn ? <HomePage /> :<FBLoginButton FBlogIn={this.FBlogIn} /> }
+        { loggedIn ? <HomePage registerForPushNotificationsAsync = {this.registerForPushNotificationsAsync} /> :<FBLoginButton FBlogIn={this.FBlogIn} /> }
       </View>
     );
   }
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
+import { Notifications } from 'expo';
 import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
 
 export default class HomePage extends Component {
   constructor(props){
     super(props);
-    this.closeApp = this.closeApp.bind(this);
+    this.state = {
+      notification: {},
+    };
   }
 
-  closeApp() {
+  componentDidMount() {
+    this.props.registerForPushNotificationsAsync();
 
+    // Handle notifications that are received or selected while the app
+    // is open. If the app was closed and then opened by tapping the
+    // notification (rather than just tapping the app icon to open it),
+    // this function will fire on the next tick after the app starts
+    // with the notification data.
+    this._notificationSubscription = Notifications.addListener(this._handleNotification);
   }
+
+  _handleNotification = (notification) => {
+    this.setState({notification: notification});
+  };
 
   render(){
     return(
       <View>
         <Text style={styles.buttonText}>Welcome to Noteefy app</Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text>Origin: {this.state.notification.origin}</Text>
+          <Text>Data: {JSON.stringify(this.state.notification.data)}</Text>
+        </View>
         <TouchableOpacity style={styles.button} onPress={this.closeApp}>
           <Text style={styles.buttonText}>Close App</Text>
         </TouchableOpacity>
@@ -45,5 +63,11 @@ const styles = StyleSheet.create({
     color: 'rgba(255,255,255, 0.9)',
     alignSelf: 'center',
     fontWeight: 'bold',
+  },
+  titleText: {
+    color: 'rgba(255,255,255, 0.9)',
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    padding:20
   }
 });
